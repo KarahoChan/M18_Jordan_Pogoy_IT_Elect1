e32496eae380a8988c18cefff730d0b9894f2a08
JordanPogs <7hello1hai@gmail.com>
2025-09-17 11:49:59 +0800

    First Commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+import CounterApp from './CounterApp';
+import ColorChangerApp from './ColorChangerApp';
+
+
+const App = () => {
+  return (
+    <>
+      <CounterApp/>
+      <ColorChangerApp />
+    </>
+  );
+};
+export default App;
